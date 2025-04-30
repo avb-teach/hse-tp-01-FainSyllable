4db3b98bcc08781eeb848252b21e7059d56353e1
FainSyllable <fedlebed04@gmail.com>
2025-04-30 21:55:01 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -25,24 +25,35 @@ generate_unique_name() {
     local ext="$3"
     local counter=1
     
-    while [ -f "${base_path}/${name}_${counter}.${ext}" ]; do
-        ((counter++))
-    done
-    
-    echo "${name}_${counter}.${ext}"
+    if [ -z "$ext" ]; then
+        while [ -e "${base_path}/${name}_${counter}" ]; do
+            ((counter++))
+        done
+        echo "${name}_${counter}"
+    else
+        while [ -e "${base_path}/${name}_${counter}.${ext}" ]; do
+            ((counter++))
+        done
+        echo "${name}_${counter}.${ext}"
+    fi
 }
 
-for file in "$input_dir"/*.txt; do
-    [ -e "$file" ] || continue
+process_file() {
+    local file="$1"
+    local output_dir="$2"
     
-    filename=$(basename "$file")
-    name="${filename%.*}"
-    ext="${filename##*.}"
+    local filename=$(basename "$file")
+    local name="${filename%.*}"
+    local ext="${filename##*.}"
+    
+    if [ "$name" = "$filename" ]; then
+        ext=""
+    fi
 
     if [ -f "$output_dir/$filename" ]; then
         if cmp -s "$file" "$output_dir/$filename"; then
             echo "Файл '$filename' уже существует и идентичен - пропускаем"
-            continue
+            return
         else
             new_name=$(generate_unique_name "$output_dir" "$name" "$ext")
             cp "$file" "$output_dir/$new_name"
@@ -52,6 +63,10 @@ for file in "$input_dir"/*.txt; do
         cp "$file" "$output_dir/"
         echo "Скопирован: $filename"
     fi
+}
+
+find "$input_dir" -type f | while read -r file; do
+    process_file "$file" "$output_dir"
 done
 
-echo "Готово! Всего файлов: $(ls "$output_dir" | wc -l)"
+echo "Готово, всего файлов: $(ls "$output_dir" | wc -l)"
